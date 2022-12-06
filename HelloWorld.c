b751d44eb487ce88ac16aaeaca830691bd2654eb
SvyatoslavDombrovskyi <svyat.xxi@gmail.com>
2022-12-06 02:00:32 +0200

    Create helloWorld file with my info
    Signed-off-by: SvyatoslavDombrovskyi <svyat.xxi@gmail.com>

---
+++ HelloWorld.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("\nGroup Number 1\n");
+	printf("Svyatoslav Dombrovskyi\n");
+	printf("svyat.xxi@gmail.com\n");
+	printf("https://github.com/SvyatoslavDombrovskyi/GL_Course.git\n");
+	printf("https://drive.google.com/drive/folders/1G28JACCkA-e0uQQKeNJY9qt7HBp9angg?usp=share_link\n");
+	/* code */
+	return 0;
+}
