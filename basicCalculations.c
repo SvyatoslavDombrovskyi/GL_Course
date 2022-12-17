160e91a51766cd7b83506af4ed8c9d54ff2face2
SvyatoslavDombrovskyi <svyat.xxi@gmail.com>
2022-12-17 12:54:20 +0200

    Created code for calculate equation
    Signed-off-by: SvyatoslavDombrovskyi <svyat.xxi@gmail.com>

---
+++ basicCalculations.c
@@ -0,0 +1,26 @@
+//variant 5
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+const int LOWER = 5;
+const int UPPER = 20;
+
+int main(void)
+{
+	srand(time(0));
+	double z1, z2, alfa;
+	
+	
+	alfa = (rand() % (UPPER - LOWER + 1)) + LOWER;	
+	
+	z1 = 1 - 1/4 * pow(sin(2*alfa), 2) + cos(2*alfa);
+	z2 = pow(cos(alfa), 2) + pow(cos(alfa), 4);
+
+	printf("\n alfa = %f\n", alfa);
+	printf(" z1 = %f\n", z1);
+	printf(" z2 = %f\n", z2);
+	return 0;
+}
