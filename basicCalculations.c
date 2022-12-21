9f0f8549d2252aef841c6663f32178876f6fe886
SvyatoslavDombrovskyi <svyat.xxi@gmail.com>
2022-12-21 14:03:33 +0200

    Used Clang-format for files
    Signed-off-by: SvyatoslavDombrovskyi <svyat.xxi@gmail.com>

--- basicCalculations.c
+++ basicCalculations.c
@@ -1,8 +1,8 @@
-//variant 5
+// variant 5
 
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 #include <time.h>
 
 const int LOWER = 5;
@@ -10,17 +10,16 @@ const int UPPER = 20;
 
 int main(void)
 {
-	srand(time(0));
-	double z1, z2, alfa;
-	
-	
-	alfa = (rand() % (UPPER - LOWER + 1)) + LOWER;	
-	
-	z1 = 1 - 1/4 * pow(sin(2*alfa), 2) + cos(2*alfa);
-	z2 = pow(cos(alfa), 2) + pow(cos(alfa), 4);
+    srand(time(0));
+    double z1, z2, alfa;
 
-	printf("\n alfa = %f\n", alfa);
-	printf(" z1 = %f\n", z1);
-	printf(" z2 = %f\n", z2);
-	return 0;
+    alfa = (rand() % (UPPER - LOWER + 1)) + LOWER;
+
+    z1 = 1 - 1 / 4 * pow(sin(2 * alfa), 2) + cos(2 * alfa);
+    z2 = pow(cos(alfa), 2) + pow(cos(alfa), 4);
+
+    printf("\n alfa = %f\n", alfa);
+    printf(" z1 = %f\n", z1);
+    printf(" z2 = %f\n", z2);
+    return 0;
 }
